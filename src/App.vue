88fcde7da2279c2e4bc8e1bea260dc8d94a0a7ac
Angel <angelinfanti@gmail.com>
2019-05-03 12:43:12 -0400

    Fixes por la URL

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     #app
-        img(src='./assets/logo.png')
+        img(src='https://camolio.github.io/angelmusic/dist/logo.png')
         h1 angelMusic
         select(v-model="selectedCountry")
             option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
