703beb30027ff131d048a6ea22c5f53152b5609b
Angel <angelinfanti@gmail.com>
2019-05-03 14:09:47 -0400

    Fixes por la URL3

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import config from './config.js'
 const apiKey = config.apiKey // == const { apiKey } = config 
-const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=venezuela&api_key=ca37cbcdd9463bd48e8a8dd7457cc078&format=json`
+const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=venezuela&api_key=ca37cbcdd9463bd48e8a8dd7457cc078&format=json
+http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&api_key=YOUR_API_KEY&format=json`
 
 export default function getArtists(country){
 	const url = URL.replace(':country', country )
